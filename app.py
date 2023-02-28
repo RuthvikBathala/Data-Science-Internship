f16ac406bcfaa30b8d419d36f91cb96d93f10866
Ruthvik <98219766+Ruthvik-B@users.noreply.github.com>
2023-02-28 16:49:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import streamlit as st 
-st.header("Hello World!")
+
+st.title(":green[Innomatics] Data App:smiley:")
+st.header("Data Science Internship")
+bt=st.button("Click here")
+
+if bt==True:
+    st.success(":blue[Congratulations]!	:tada:	:confetti_ball:",icon="✅")
+    st.snow()
+else:
+    st.balloons()
+    
